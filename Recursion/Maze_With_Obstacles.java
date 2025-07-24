ee8bad11bf1334889b8b20ccb34484a5fb688854
Shridhi Gupta <149915524+ShridhiGupta@users.noreply.github.com>
2025-07-24 22:57:05 +0530

    Create Maze_With_Obstacles.java

---
+++ Recursion/Maze_With_Obstacles.java
@@ -0,0 +1,25 @@
+public class Maze_With_Obstacles {
+    public static void main(String[] args) {
+        boolean[][] board = {
+            {true, true, true},
+            {true, false, true},
+            {true, true, true}
+        };
+        pathRestriction("", board, 0, 0);
+    }
+    static void pathRestriction(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c]) {
+            return;
+        }
+        if(r < maze.length - 1) {
+            pathRestriction(p + "D", maze, r + 1, c);
+        }   
+        if(c < maze[0].length - 1) {
+            pathRestriction(p + "R", maze, r, c + 1);
+        }
+    }
+}

762daaa4d7e7676b030d321ab7e031d4d048755d
Shridhi Gupta <149915524+ShridhiGupta@users.noreply.github.com>
2025-08-09 09:34:52 +0530

    Update BinarySearch.java
    Edited the mistakes.

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 public class BinarySearch{
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number of elements:");
-        int n = in.nextInt();
-        System.out.println("Enter the elements:-");
-        for(int i=0;i<n;i++){
-
+    private static int binarySearch(int[] nums, int s, int e){
+        while(s<e){
+            int mid = s+(e-s)/2;
+            if(nums[mid] < target) s++;
+            else if(nums[mid] > target) e--;
+            else return mid;
         }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int target = 5;
+        int s = 0;
+        int e = arr.length-1;
+        int ans = binarySearch(arr, s, e);
+        System.out.println("Element found at: ", ans);
     }
 }
